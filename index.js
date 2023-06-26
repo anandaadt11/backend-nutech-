0169ba0818ee21f5fd3de8c937340185bf2c10d2
Ananda Aditya <anandaadityaputra11@gmail.com>
2023-06-26 20:50:46 +0700

    fix index.js

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.use(router);
 app.use(ProductRoutes);
 app.use(express.static("public"));
 
-app.listen(5000, () => {
+app.listen(9002, () => {
   console.log("Server is running on port 5000");
 });
