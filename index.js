2353e7b18c1410a16ede86b6f1c058add9f9395c
Ananda Aditya <anandaadityaputra11@gmail.com>
2023-07-04 11:56:04 +0700

    change port to 8080

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.use(cookieParser());
 app.use(router);
 app.use(ProductRoutes);
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
 });
